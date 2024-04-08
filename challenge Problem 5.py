40c00e78dfa8ea11b0d8bbbfadff6620e2a18425
Watterpstra <165731534+Watterpstra@users.noreply.github.com>
2024-04-08 14:36:49 -0400

    Add files via upload
    New version of the code, doesn't have an iterator needed for user input

--- challenge Problem 5.py
+++ challenge Problem 5.py
@@ -1,22 +1,5 @@
-'''
-example to be made recursively
 
-    def s(word_list, word):
-  i = 0
-  while i < len(word_list):
-    if word == word_list[i]:
-      print(i)
-      break
-    elif word < word_list[i]:
-      print("Fail")
-      break
-    else: i += 1
-  if i >= len(word_list):
-    print("Fail")
-
-'''
-
-def s(word_list, word, num):
+def s(word_list, word, num=0):
 
     #Basecase
     if len(word_list) < 1:
@@ -24,12 +7,12 @@ def s(word_list, word, num):
 
     #recursive statement
     if word == word_list[0]:
-        return f'{word}, {num}'
+        print(num)
+        return
     else:
         return s(word_list[1:], word, num + 1)
 
 
 if __name__ == '__main__':
     words = ["Are", "Body", "Career", "Computer", "Dam", "Mathematics", "Science", "Ugh", "Why"]
-    test = s(words, "Body", 0)
-    print(test)
+    test = s(words, "Computer")
